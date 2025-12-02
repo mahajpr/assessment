941d367f22d7e9dfa41c581af339299c28880215
mahajpr <mahajpr01@gmail.com>
2025-12-02 20:12:48 +0530

    program4.py

--- program4.py
+++ program4.py
@@ -3,10 +3,13 @@ client = Groq(api_key="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"
 response = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=[
-            {"role":"system","content":"i is sick"},
-            {"role":"user","content":"i am sick"},
-            {"role":"system","content":"check the grammer correction"},
+            {"role":"system","content":"you are a helpful assistant that correct english grammer "},
+            {"role":"user","content":"i is sick"},
+            {"role":"assistant","content":"i am sick"},
+            {"role":"user","content":"she go to school yesterday"},
+            {"role":"assistant","content":"she went to school yesterday"},
             {"role":"user","content":"there a cow the valley"}
         ]
 )
+
 print(response.choices[0].message.content)
