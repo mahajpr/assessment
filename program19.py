12c0ff99f996829117e16054440317b7d24f9590
mahajpr <mahajpr01@gmail.com>
2025-12-02 20:51:50 +0530

    program19.py

--- program19.py
+++ program19.py
@@ -1,9 +1,13 @@
-from groq import Groq
+rom groq import Groq
 client = Groq(api_key="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")
 response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[{"role":"system","content":"In the generated answer the word limit should be 200, It should be friendly manner, the wording should  be in paragraph and the output format will be in  json "},
-        {"role":"user","content":"give the email for reminder of the policy holder"}
-        ]
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {"role": "system","content": "Follow these conditions:\n"
+                                     "Word limit: 200 words\n"
+                                     "Tone: professional\n"
+                                     "Style: paragraph writing\n"
+                                     "Output format: JSON only\n"},
+        {"role": "user","content": "Give an email reminder for the policy holder."}]
 )
 print(response.choices[0].message.content)
