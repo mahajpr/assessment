0bae4b8d708b2f845e0cc498d56b94d2ff924829
mahajpr <mahajpr01@gmail.com>
2025-12-02 19:09:29 +0530

    program1.py

--- program1.py
+++ program1.py
@@ -1,12 +1,17 @@
-
 from groq import Groq
 client = Groq(api_key="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")
-response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[{"role":"user","content":"give me a receipe of sandwich"}
-        ]
-)
 with open("program.txt","r") as f:
     content=f.read()
+    topic = "sandwich recipe"
+    tone = "friendly"
+    final_prompt = content.format(topic=topic, tone=tone)
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[{"role":"user","content":final_prompt}
+        ]
+)
 print(content)
+print(final_prompt)
 print(response.choices[0].message.content)
+
+
