09d44082aea19187f6cc67870d6c70ae8e47e33b
mahajpr <mahajpr01@gmail.com>
2025-12-02 20:36:17 +0530

    program5.py

--- program5.py
+++ program5.py
@@ -1,9 +1,13 @@
 from groq import Groq
 client = Groq(api_key="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")
+puzzle="you bought 10 tennis balls.you give 5 tennis balls to your friend and another 2 tennis balls to your sister. how many tennis balls you have remaining"
 response = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
-      
-        messages=[{"role":"user","content":"write the 3 paragraphs about is the wild life is safe or not for humans "}],
-        
+        messages=[{"role":"system","content":"while you answering a question always use this format"
+                "Thought: explain your reasoning.\n"
+                "Action: describe the calculation.\n"
+                "Result: give the final answer.\n"},
+                {"role":"user","content":f"solve this puzzle using thought,action and result format {puzzle}"}]
 )
+print(puzzle)
 print(response.choices[0].message.content)
